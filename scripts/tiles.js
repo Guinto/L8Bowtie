84b89985f0ccda3584bdce386e8801d4b94e98f6
Trent Ellingsen <trentellingsen@gmail.com>
2012-09-25 16:15:19 -0700

    Turn on and off are now working
    Made a tile object that handles stuff nicely :D

--- scripts/tiles.js
+++ scripts/tiles.js
@@ -2,13 +2,35 @@ var rowID = 0;
 var tileID = 0;
 var savedPalette = [];
 var currColor = "";
+var offColor = "#292929";
+var tiles = new Array();
+
+function Tile(id) {
+	this.lit = false;
+	this.currColor = offColor;
+	this.tileID = id;
+	
+	this.changeColor = function(color) {
+		this.lit = true;
+		this.currColor = color;
+		$('#tile' + this.tileID).css('background', this.currColor);
+	};
+	
+	this.turnOff = function() {
+		this.lit = false;
+		this.currColor = offColor;
+		$('#tile' + this.tileID).css('background', this.currColor);
+	};
+}
 
 function setupColorPickerWithSelector(selector) {
 	$(selector).spectrum({
+		showInput: true,
+		preferredFormat: "hex",
 		showInitial: true,
-		showPalette: true,
+		showPalette: true, // Needed to show premade palettte
+		palette: ["#255ea6", "#da3838"],
 		showSelectionPalette: true,
-		palette: [],
 		localStorageKey: "spectrum.homepage",
 		change: function(color) {
 			currColor = color.toHexString();
@@ -20,28 +42,35 @@ function setupEvents() {
 	var $tiles = $('.tile');
 	
 	$tiles.on('click', function() {
-		$(this).css('background', currColor);
-	});
-}
-
-function setupTouchEvents() {
-	var $tiles = $('.tile');
-	
-	$tiles.spectrum({
-		showInitial: true,
-		showPalette: true,
-		showSelectionPalette: true,
-		palette: [],
-		localStorageKey: "spectrum.homepage",
-		change: function(color) {
-			$(this).css("background", color.toHexString());
-			currColor = color.toHexString();
+		var id = $(this).attr("id");
+		id = id.substring(4, id.length);
+		var tile = getTile(id);
+		
+		if (tile === null) {
+			console.log("Tile with id " + id + " does not exist");
+			return;
+		}
+		
+		if (!tile.lit || tile.currColor !== currColor) {
+			tile.changeColor(currColor);
+		} else {
+			tile.turnOff();
 		}
 	});
 }
 
+function getTile(id) {
+	for (i in tiles) {
+		if (tiles[i].tileID == id) {
+			return tiles[i];
+		}
+	}
+	return null;
+}
+
 function createTileAndAppendTo(selector) {
 	$(selector).append('<div id="tile' + tileID + '" class="tile"></div>');
+	tiles.push(new Tile(tileID));
 	tileID++;
 }
 
